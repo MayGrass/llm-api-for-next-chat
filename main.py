840623e58abe561971f2e9b0f9e02573d06ede98
MayGrass <15877196+MayGrass@users.noreply.github.com>
2025-04-17 16:05:24 +0800

    feat: support new gpt model, include o4 -mini and gpt-4.1

--- main.py
+++ main.py
@@ -125,7 +125,8 @@ async def openai_chat_completions(
         [jsonable_encoder(message, exclude_unset=True) for message in comletions_json_data.messages], ensure_ascii=False
     )
 
-    if model.startswith("gpt") or model.startswith("o1") or model.startswith("o3"):
+    supported_prefixes = {"gpt", "o1", "o3", "o4"}
+    if any(model.startswith(prefix) for prefix in supported_prefixes):
         if not env.bool("USE_CHATGPT_WEB", True):
             # reverse to openai completions
             openai_api = env("OPENAI_API_URL", "https://api.openai.com/v1/chat/completions")
